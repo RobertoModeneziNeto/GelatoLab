e51dae5a83fdc46897aaaf1e5c2e032784135fed
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-06 14:19:15 -0300

    Create Movimentacoes

---
+++ Models/Movimentacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelatoLab.Models
+{
+    public class Movimentacoes
+    {
+        public int IdMovimentacao { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataMovimentacao { get; set; }
+        public Produtos Produto { get; set; }
+        public int Quantidade { get; set; }
+        public string Observacao { get; set; }
+    }
+}
