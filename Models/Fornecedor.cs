8c00d1b1794a1be5a750346e9718c859751e45a4
Murilo Turcato Piai <murilotpiai@gmail.com>
2025-11-05 21:45:31 -0300

    Update Fornecedor.cs

--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -8,5 +8,10 @@ namespace GelatoLab.Models
 {
     public class Fornecedor
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
     }
 }
