e66cedc568742e73f3bf28784c5f67849d122de0
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-06 14:57:06 -0300

    Create DataBaseSqlServer.cs

---
+++ Services/DataBaseSqlServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelatoLab.Services
+{
+    public class DataBaseSqlServer
+    {
+        private SqlConnection GetConnection()
+        {
+            //Variavel q ira armazenar a conexão
+            SqlConnection connection = new SqlConnection();
+
+            //Definindo a string de conexão
+            //Dados para conectar co o servidor
+            //Precisamos do Host (Nome/IP)
+            //Nome do banco de dados
+            //Autenticação (Usuario e Senha ou Autenticação do Windows)
+            connection.ConnectionString =
+                "Data Source=DESKTOP-64RILEV\\SQLEXPRESS;" + //Nome do Servidor (RAPAZIADA MUDAR O NOME DO SERVIDOR AQUI)
+                "Initial Catalog=GelatoSql;" + //Nome do Banco
+                "Integrated Security=SSPI;";// Autenticação pelo Windows
+
+            //Abrir a conexão com o banco
+            connection.Open();
+            //Retornar a conexão aberta
+            return connection;
+        }
+
+
+        //Metodo Publico para a execução de comandos
+        //de manipulação (Insert, Update, Delete)
+        //Retornar a quantidade de linhas afetadas
+        public int ExecuteSQL(SqlCommand command)
+        {
+            //Recuperar a conexão com o banco
+            command.Connection = GetConnection();
+            //Executar o comando e retornar o resultado
+            return command.ExecuteNonQuery();
+        }
+
+        //Método publico que executa comando SELECT no banco de dados
+        //Retorna apenas a primeira coluna da primeira linha do banco
+        //Utilizamos o tipo de dados object pois não sabemos o tipo
+        //de dados que será retornado
+        public object ExecuteScalar(SqlCommand command)
+        {
+            command.Connection = GetConnection();
+            return command.ExecuteScalar();
+        }
+
+        //Método publico que executa comando SQL 
+        //Retorna todas as linhas e colunas da consult
+        public DataTable GetDataTable(SqlCommand command)
+        {
+            // Criando o objeto DataTable
+            DataTable dataTable = new DataTable();
+
+            // Definindo a conexão do comando SQL
+            command.Connection = GetConnection();
+            // Criando o objeto SqlDataAdapter e vinculando o comando
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+
+            // Preenchendo o DataTable com os dados do banco
+            sqlDataAdapter.Fill(dataTable);
+
+            // Retornando o DataTable preenchido
+            return dataTable;
+        }
+
+    }
+}
