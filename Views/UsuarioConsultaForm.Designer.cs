276fe3b30f0270a89ddd47a7c4e53f9c8ed10ae3
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-10 16:53:11 -0300

    Tela UsuarioConsulta Criada

---
+++ Views/UsuarioConsultaForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GelatoLab.Views
+{
+    partial class UsuarioConsultaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.lblPesquisar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(701, 392);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 16;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AllowUserToAddRows = false;
+            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(41, 110);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            this.dgvUsuarios.RowHeadersWidth = 51;
+            this.dgvUsuarios.RowTemplate.Height = 24;
+            this.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsuarios.Size = new System.Drawing.Size(666, 254);
+            this.dgvUsuarios.TabIndex = 15;
+            this.dgvUsuarios.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsuarios_CellDoubleClick);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(701, 64);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 14;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(701, 35);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 13;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(101, 35);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(579, 22);
+            this.txtPesquisar.TabIndex = 12;
+            // 
+            // lblPesquisar
+            // 
+            this.lblPesquisar.AutoSize = true;
+            this.lblPesquisar.Location = new System.Drawing.Point(24, 38);
+            this.lblPesquisar.Name = "lblPesquisar";
+            this.lblPesquisar.Size = new System.Drawing.Size(71, 16);
+            this.lblPesquisar.TabIndex = 11;
+            this.lblPesquisar.Text = "Pesquisar:";
+            // 
+            // UsuarioConsultaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.lblPesquisar);
+            this.Name = "UsuarioConsultaForm";
+            this.Text = "UsuarioConsultaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Label lblPesquisar;
+    }
+}
---
+++ Views/UsuarioConsultaForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GelatoLab.Models;
+using GelatoLab.Controllers;
+
+namespace GelatoLab.Views
+{
+    public partial class UsuarioConsultaForm : Form
+    {
+        UsuarioController controller = new UsuarioController();
+        public UsuarioConsultaForm()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        // ============================================================
+        // CARREGAR GRID INICIAL
+        // ============================================================
+        private void CarregarGrid()
+        {
+            UsuarioCollection lista = controller.GetAll();
+            dgvUsuarios.DataSource = lista;
+
+            dgvUsuarios.Columns["IdUsuario"].HeaderText = "ID";
+            dgvUsuarios.Columns["Nome"].HeaderText = "Nome";
+            dgvUsuarios.Columns["Login"].HeaderText = "Login";
+            dgvUsuarios.Columns["Senha"].HeaderText = "Senha";
+            dgvUsuarios.Columns["Tipo"].HeaderText = "Tipo";
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string filtro = txtPesquisar.Text.Trim();
+
+            if (filtro == "")
+            {
+                MessageBox.Show("Digite um nome ou login para pesquisar.",
+                                "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Primeiro tenta pelo nome
+            UsuarioCollection lista = controller.GetByName(filtro);
+
+            // Se não achou nada, tenta pelo login
+            if (lista.Count == 0)
+                lista = controller.GetByLogin(filtro);
+
+            dgvUsuarios.DataSource = lista;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            CarregarGrid();
+        }
+
+        private void dgvUsuarios_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            int idUsuario = (int)dgvUsuarios.Rows[e.RowIndex].Cells["IdUsuario"].Value;
+
+            UsuarioCadastroForm tela = new UsuarioCadastroForm();
+            tela.CarregarUsuario(idUsuario);
+            tela.ShowDialog();
+
+            // Atualiza após edição
+            CarregarGrid();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
