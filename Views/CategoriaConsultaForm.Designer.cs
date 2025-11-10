5bcf4c0260f0aea09df82930ccf46a0959cddc89
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-09 22:26:53 -0300

    Tela CategoriaConsulta Criada

---
+++ Views/CategoriaConsultaForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GelatoLab.Views
+{
+    partial class CategoriaConsultaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgCategorias = new System.Windows.Forms.DataGridView();
+            this.lblPesquisar = new System.Windows.Forms.Label();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCategorias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgCategorias
+            // 
+            this.dvgCategorias.AllowUserToAddRows = false;
+            this.dvgCategorias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dvgCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgCategorias.Location = new System.Drawing.Point(45, 97);
+            this.dvgCategorias.Name = "dvgCategorias";
+            this.dvgCategorias.ReadOnly = true;
+            this.dvgCategorias.RowHeadersWidth = 51;
+            this.dvgCategorias.RowTemplate.Height = 24;
+            this.dvgCategorias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dvgCategorias.Size = new System.Drawing.Size(666, 254);
+            this.dvgCategorias.TabIndex = 0;
+            this.dvgCategorias.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgCategorias_CellDoubleClick);
+            // 
+            // lblPesquisar
+            // 
+            this.lblPesquisar.AutoSize = true;
+            this.lblPesquisar.Location = new System.Drawing.Point(12, 9);
+            this.lblPesquisar.Name = "lblPesquisar";
+            this.lblPesquisar.Size = new System.Drawing.Size(71, 16);
+            this.lblPesquisar.TabIndex = 1;
+            this.lblPesquisar.Text = "Pesquisar:";
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(89, 6);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(579, 22);
+            this.txtPesquisar.TabIndex = 2;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(689, 6);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 3;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(689, 35);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 4;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(689, 379);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 5;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // CategoriaConsultaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.lblPesquisar);
+            this.Controls.Add(this.dvgCategorias);
+            this.Name = "CategoriaConsultaForm";
+            this.Text = "CategoriaConsultaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCategorias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgCategorias;
+        private System.Windows.Forms.Label lblPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
---
+++ Views/CategoriaConsultaForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GelatoLab.Models;
+using GelatoLab.Controllers;
+
+namespace GelatoLab.Views
+{
+    public partial class CategoriaConsultaForm : Form
+    {
+        CategoriaController controller = new CategoriaController();
+        public CategoriaConsultaForm()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        private void CarregarGrid()
+        {
+            CategoriaCollection lista = controller.GetAll();
+            dvgCategorias.DataSource = lista;
+
+            // Renomeando colunas
+            dvgCategorias.Columns["IdCategoria"].HeaderText = "ID";
+            dvgCategorias.Columns["Nome"].HeaderText = "Nome";
+            dvgCategorias.Columns["Descricao"].HeaderText = "Descrição";
+            dvgCategorias.Columns["Ativo"].HeaderText = "Ativo";
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string filtro = txtPesquisar.Text.Trim();
+
+            if (filtro == "")
+            {
+                MessageBox.Show("Digite algo para pesquisar.",
+                                "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            CategoriaCollection lista = controller.GetByName(filtro);
+            dvgCategorias.DataSource = lista;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            CarregarGrid();
+        }
+
+        private void dvgCategorias_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            int id = (int)dvgCategorias.Rows[e.RowIndex].Cells["IdCategoria"].Value;
+
+            CategoriaCadastroForm tela = new CategoriaCadastroForm();
+            tela.CarregarCategoria(id);
+            tela.ShowDialog();
+
+            // Recarregar lista depois de editar
+            CarregarGrid();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
