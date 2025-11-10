a925c084dcf8aad733d25bad44379029873ff5c8
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-09 23:54:30 -0300

    Tela ProdutosCadastro Criada

---
+++ Views/ProdutoCadastroForm.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace GelatoLab.Views
+{
+    partial class ProdutoCadastroForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtCodBarras = new System.Windows.Forms.TextBox();
+            this.lblCodBarras = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.cbTipoUnidade = new System.Windows.Forms.ComboBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.cbFornecedor = new System.Windows.Forms.ComboBox();
+            this.lblFornecedor = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(12, 9);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(23, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(41, 6);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(215, 6);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 22);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(421, 6);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(100, 22);
+            this.txtQuantidade.TabIndex = 5;
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.Location = new System.Drawing.Point(335, 9);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(80, 16);
+            this.lblQtd.TabIndex = 4;
+            this.lblQtd.Text = "Quantidade:";
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(592, 6);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(100, 22);
+            this.txtPreco.TabIndex = 7;
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(540, 9);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(46, 16);
+            this.lblPreco.TabIndex = 6;
+            this.lblPreco.Text = "Preço:";
+            // 
+            // txtCodBarras
+            // 
+            this.txtCodBarras.Location = new System.Drawing.Point(129, 49);
+            this.txtCodBarras.Name = "txtCodBarras";
+            this.txtCodBarras.Size = new System.Drawing.Size(100, 22);
+            this.txtCodBarras.TabIndex = 9;
+            // 
+            // lblCodBarras
+            // 
+            this.lblCodBarras.AutoSize = true;
+            this.lblCodBarras.Location = new System.Drawing.Point(7, 52);
+            this.lblCodBarras.Name = "lblCodBarras";
+            this.lblCodBarras.Size = new System.Drawing.Size(116, 16);
+            this.lblCodBarras.TabIndex = 8;
+            this.lblCodBarras.Text = "Código de Barras:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(269, 52);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(116, 16);
+            this.lblTipo.TabIndex = 10;
+            this.lblTipo.Text = "Código de Barras:";
+            // 
+            // cbTipoUnidade
+            // 
+            this.cbTipoUnidade.FormattingEnabled = true;
+            this.cbTipoUnidade.Location = new System.Drawing.Point(391, 47);
+            this.cbTipoUnidade.Name = "cbTipoUnidade";
+            this.cbTipoUnidade.Size = new System.Drawing.Size(121, 24);
+            this.cbTipoUnidade.TabIndex = 11;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(12, 119);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(69, 16);
+            this.lblCategoria.TabIndex = 12;
+            this.lblCategoria.Text = "Categoria:";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Location = new System.Drawing.Point(88, 116);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(121, 24);
+            this.cbCategoria.TabIndex = 13;
+            // 
+            // cbFornecedor
+            // 
+            this.cbFornecedor.FormattingEnabled = true;
+            this.cbFornecedor.Location = new System.Drawing.Point(323, 119);
+            this.cbFornecedor.Name = "cbFornecedor";
+            this.cbFornecedor.Size = new System.Drawing.Size(121, 24);
+            this.cbFornecedor.TabIndex = 15;
+            // 
+            // lblFornecedor
+            // 
+            this.lblFornecedor.AutoSize = true;
+            this.lblFornecedor.Location = new System.Drawing.Point(247, 122);
+            this.lblFornecedor.Name = "lblFornecedor";
+            this.lblFornecedor.Size = new System.Drawing.Size(80, 16);
+            this.lblFornecedor.TabIndex = 14;
+            this.lblFornecedor.Text = "Fornecedor:";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(526, 214);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 19;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(428, 214);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 18;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(326, 214);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 17;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(200, 214);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 16;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // ProdutoCadastroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.cbFornecedor);
+            this.Controls.Add(this.lblFornecedor);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.cbTipoUnidade);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.txtCodBarras);
+            this.Controls.Add(this.lblCodBarras);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Name = "ProdutoCadastroForm";
+            this.Text = "ProdutoCadastroForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label lblQtd;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.TextBox txtCodBarras;
+        private System.Windows.Forms.Label lblCodBarras;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox cbTipoUnidade;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.ComboBox cbFornecedor;
+        private System.Windows.Forms.Label lblFornecedor;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
---
+++ Views/ProdutoCadastroForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GelatoLab.Models;
+using GelatoLab.Controllers;
+
+namespace GelatoLab.Views
+{
+    public partial class ProdutoCadastroForm : Form
+    {
+
+        ProdutosController produtoController = new ProdutosController();
+        CategoriaController categoriaController = new CategoriaController();
+        FornecedorController fornecedorController = new FornecedorController();
+        public ProdutoCadastroForm()
+        {
+            InitializeComponent();
+            CarregarComboCategoria();
+            CarregarComboFornecedor();
+            CarregarComboTipoUnidade();
+        }
+
+        // ============================================================
+        // CARREGAR COMBO DE CATEGORIA
+        // ============================================================
+        private void CarregarComboCategoria()
+        {
+            var categorias = categoriaController.GetAtivas();
+            cbCategoria.DataSource = categorias;
+            cbCategoria.DisplayMember = "Nome";
+            cbCategoria.ValueMember = "IdCategoria";
+        }
+
+        // ============================================================
+        // CARREGAR COMBO DE FORNECEDOR
+        // ============================================================
+        private void CarregarComboFornecedor()
+        {
+            var fornecedores = fornecedorController.GetAll();
+            cbFornecedor.DataSource = fornecedores;
+            cbFornecedor.DisplayMember = "Nome";
+            cbFornecedor.ValueMember = "IdFornecedor";
+        }
+
+        // CARREGAR TIPO DE UNIDADE
+        // ============================================================
+        private void CarregarComboTipoUnidade()
+        {
+            cbTipoUnidade.Items.Add("Unidade");
+            cbTipoUnidade.Items.Add("Caixa");
+            cbTipoUnidade.SelectedIndex = 0;
+        }
+
+
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.Trim() == "")
+            {
+                MessageBox.Show("O nome do produto é obrigatório.",
+                                "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Produtos produto = new Produtos();
+            produto.Nome = txtNome.Text.Trim();
+            produto.Quantidade = int.Parse(txtQuantidade.Text);
+            produto.Preco = decimal.Parse(txtPreco.Text);
+            produto.codBarras = txtCodBarras.Text.Trim();
+            produto.TipoUnidade = cbTipoUnidade.Text;
+
+            produto.Categoria = new Categoria()
+            {
+                IdCategoria = (int)cbCategoria.SelectedValue
+            };
+
+            produto.Fornecedor = new Fornecedor()
+            {
+                IdFornecedor = (int)cbFornecedor.SelectedValue
+            };
+
+            // Se ainda não tem ID → novo produto
+            if (txtId.Text == "")
+            {
+                produtoController.Inserir(produto);
+                MessageBox.Show("Produto cadastrado com sucesso!");
+            }
+            else
+            {
+                produto.IdProduto = int.Parse(txtId.Text);
+                produtoController.Alterar(produto);
+                MessageBox.Show("Produto alterado com sucesso!");
+            }
+
+            LimparCampos();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text == "")
+            {
+                MessageBox.Show("Selecione um produto antes de excluir.");
+                return;
+            }
+
+            int id = int.Parse(txtId.Text);
+            produtoController.Excluir(id);
+
+            MessageBox.Show("Produto excluído com sucesso!");
+            LimparCampos();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtId.Clear();
+            txtNome.Clear();
+            txtPreco.Clear();
+            txtQuantidade.Clear();
+            txtCodBarras.Clear();
+
+            cbCategoria.SelectedIndex = 0;
+            cbFornecedor.SelectedIndex = 0;
+            cbTipoUnidade.SelectedIndex = 0;
+        }
+
+        // ============================================================
+        // CARREGAR PRODUTO PARA EDIÇÃO
+        // ============================================================
+        public void CarregarProduto(int idProduto)
+        {
+            Produtos produto = produtoController.GetById(idProduto);
+
+            if (produto != null)
+            {
+                txtId.Text = produto.IdProduto.ToString();
+                txtNome.Text = produto.Nome;
+                txtQuantidade.Text = produto.Quantidade.ToString();
+                txtPreco.Text = produto.Preco.ToString("F2");
+                txtCodBarras.Text = produto.codBarras;
+                cbTipoUnidade.Text = produto.TipoUnidade;
+
+                cbCategoria.SelectedValue = produto.Categoria.IdCategoria;
+                cbFornecedor.SelectedValue = produto.Fornecedor.IdFornecedor;
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
