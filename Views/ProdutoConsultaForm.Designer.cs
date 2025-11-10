d954fa67da99403918a2c84e51d4ef8eb92d235e
Roberto Modenezi Neto <robertomodenezi@gmail.com>
2025-11-10 00:02:06 -0300

    Tela ProdutoConsulta criada

---
+++ Views/ProdutoConsultaForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GelatoLab.Views
+{
+    partial class ProdutoConsultaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.lblPesquisar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(701, 392);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 16;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.AllowUserToAddRows = false;
+            this.dgvProdutos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(41, 110);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.ReadOnly = true;
+            this.dgvProdutos.RowHeadersWidth = 51;
+            this.dgvProdutos.RowTemplate.Height = 24;
+            this.dgvProdutos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProdutos.Size = new System.Drawing.Size(666, 254);
+            this.dgvProdutos.TabIndex = 15;
+            this.dgvProdutos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProdutos_CellDoubleClick);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(701, 64);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 14;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(701, 35);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 13;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(101, 35);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(579, 22);
+            this.txtPesquisar.TabIndex = 12;
+            // 
+            // lblPesquisar
+            // 
+            this.lblPesquisar.AutoSize = true;
+            this.lblPesquisar.Location = new System.Drawing.Point(24, 38);
+            this.lblPesquisar.Name = "lblPesquisar";
+            this.lblPesquisar.Size = new System.Drawing.Size(71, 16);
+            this.lblPesquisar.TabIndex = 11;
+            this.lblPesquisar.Text = "Pesquisar:";
+            // 
+            // ProdutoConsultaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.dgvProdutos);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.lblPesquisar);
+            this.Name = "ProdutoConsultaForm";
+            this.Text = "ProdutoConsultaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Label lblPesquisar;
+    }
+}
---
+++ Views/ProdutoConsultaForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GelatoLab.Models;
+using GelatoLab.Controllers;
+
+namespace GelatoLab.Views
+{
+    public partial class ProdutoConsultaForm : Form
+    {
+        ProdutosController produtoController = new ProdutosController();
+        public ProdutoConsultaForm()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        // ============================================================
+        // CARREGAR GRID
+        // ============================================================
+        private void CarregarGrid()
+        {
+            ProdutosCollection lista = produtoController.GetAll();
+            dgvProdutos.DataSource = lista;
+
+            // Renomear colunas
+            dgvProdutos.Columns["IdProduto"].HeaderText = "ID";
+            dgvProdutos.Columns["Nome"].HeaderText = "Produto";
+            dgvProdutos.Columns["Quantidade"].HeaderText = "Qtd";
+            dgvProdutos.Columns["Preco"].HeaderText = "Preço";
+            dgvProdutos.Columns["codBarras"].HeaderText = "Código de Barras";
+            dgvProdutos.Columns["TipoUnidade"].HeaderText = "Unidade";
+
+            // Ocultar objetos Categoria e Fornecedor
+            dgvProdutos.Columns["Categoria"].Visible = false;
+            dgvProdutos.Columns["Fornecedor"].Visible = false;
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string texto = txtPesquisar.Text.Trim();
+
+            if (texto == "")
+            {
+                MessageBox.Show("Digite um nome ou código de barras para pesquisar.",
+                                "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ProdutosCollection lista;
+
+            // Pesquisar por nome
+            lista = produtoController.GetByName(texto);
+
+            // Se não achou, tentar por código de barras
+            if (lista.Count == 0)
+                lista = produtoController.GetByCodBarras(texto);
+
+            dgvProdutos.DataSource = lista;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            CarregarGrid();
+        }
+
+        private void dgvProdutos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            int idProduto = (int)dgvProdutos.Rows[e.RowIndex].Cells["IdProduto"].Value;
+
+            ProdutoCadastroForm tela = new ProdutoCadastroForm();
+            tela.CarregarProduto(idProduto);
+            tela.ShowDialog();
+
+            CarregarGrid(); // Atualiza lista depois da edição
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
